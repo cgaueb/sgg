4f6359ee2abb015214ae24d552a647b33e6ec9f1
gsiros <geo2001s@gmail.com>
2020-11-18 18:33:09 +0200

    Added macOS support.
    Added installer shell scripts and the modified 3rdparty/MACOS_SUPPORT/SDL_stdinc.h

---
+++ build_sgg_macOS.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# @gsiros modifications
+
+CC=g++
+LD=g++
+AR=ar
+BUILD_PATH="build/Release"
+BUILD_PATH_DEBUG="build/Debug"
+LIB_PATH="lib"
+CFLAGS="-O2"
+CFLAGS_DEBUG="-Og -g"
+
+mkdir -p build
+mkdir -p $LIB_PATH
+mkdir -p $BUILD_PATH
+mkdir -p $BUILD_PATH_DEBUG
+mkdir -p $BUILD_PATH/sgg
+mkdir -p $BUILD_PATH_DEBUG/sgg
+
+echo "-- START OF SGG LIB (RELEASE) COMPILATION --"
+$CC -c -std=c++11 $CFLAGS -I3rdparty/include graphics/graphics.cpp -o $BUILD_PATH/sgg/graphics.o
+echo "Compiled graphics!"
+$CC -c -std=c++11 $CFLAGS -I3rdparty/include graphics/GLbackend.cpp -o $BUILD_PATH/sgg/GLbackend.o
+echo "Compiled GLbackend!"
+$CC -c -std=c++11 $CFLAGS -I3rdparty/include graphics/shader.cpp -o $BUILD_PATH/sgg/shader.o
+echo "Compiled shader!"
+$CC -c -std=c++11 $CFLAGS -I3rdparty/include graphics/texture.cpp -o $BUILD_PATH/sgg/texture.o
+echo "Compiled texture!"
+$CC -c -std=c++11 $CFLAGS -I3rdparty/include graphics/audio.cpp -o $BUILD_PATH/sgg/audio.o
+echo "Compiled audio!"
+$CC -c -std=c++11 $CFLAGS -I3rdparty/include graphics/AudioManager.cpp -o $BUILD_PATH/sgg/AudioManager.o
+echo "Compiled AudioManager!"
+$CC -c -std=c++11 $CFLAGS -I3rdparty/include graphics/lodepng.cpp -o $BUILD_PATH/sgg/lodepng.o
+echo "Compiled lodepng!"
+$CC -c -std=c++11 $CFLAGS -I3rdparty/include graphics/fonts.cpp -o $BUILD_PATH/sgg/fonts.o
+echo "Compiled fonts!"
+
+$AR rcs $LIB_PATH/libsgg.a $BUILD_PATH/sgg/*.o
+
+echo "-- START OF SGG LIB (DEBUG) COMPILATION --"
+$CC -c -std=c++11 $CFLAGS_DEBUG -I3rdparty/include graphics/graphics.cpp -o $BUILD_PATH_DEBUG/sgg/graphics.o
+echo "Compiled graphics!"
+$CC -c -std=c++11 $CFLAGS_DEBUG -I3rdparty/include graphics/GLbackend.cpp -o $BUILD_PATH_DEBUG/sgg/GLbackend.o
+echo "Compiled GLbackend!"
+$CC -c -std=c++11 $CFLAGS_DEBUG -I3rdparty/include graphics/shader.cpp -o $BUILD_PATH_DEBUG/sgg/shader.o
+echo "Compiled shader!"
+$CC -c -std=c++11 $CFLAGS_DEBUG -I3rdparty/include graphics/texture.cpp -o $BUILD_PATH_DEBUG/sgg/texture.o
+echo "Compiled texture!"
+$CC -c -std=c++11 $CFLAGS_DEBUG -I3rdparty/include graphics/audio.cpp -o $BUILD_PATH_DEBUG/sgg/audio.o
+echo "Compiled audio!"
+$CC -c -std=c++11 $CFLAGS_DEBUG -I3rdparty/include graphics/AudioManager.cpp -o $BUILD_PATH_DEBUG/sgg/AudioManager.o
+echo "Compiled AudioManager!"
+$CC -c -std=c++11 $CFLAGS_DEBUG -I3rdparty/include graphics/lodepng.cpp -o $BUILD_PATH_DEBUG/sgg/lodepng.o
+echo "Compiled lodepng!"
+$CC -c -std=c++11 $CFLAGS_DEBUG -I3rdparty/include graphics/fonts.cpp -o $BUILD_PATH_DEBUG/sgg/fonts.o
+echo "Compiled fonts!"
+
+$AR rcs $LIB_PATH/libsggd.a $BUILD_PATH_DEBUG/sgg/*.o
