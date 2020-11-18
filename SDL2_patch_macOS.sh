89709d5dde97dfae6cb5b55feca92b69fe6d4d5f
gsiros <geo2001s@gmail.com>
2020-11-18 22:32:54 +0200

    Removed unnecessary shell script.

--- SDL2_patch_macOS.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# @gsiros patch
-echo "Modifying SDL2_stdinc.h to fit macOS..."
-cp 3rdparty/include/SDL2/SDL_stdinc.h 3rdparty/include/SDL2/SDL_stdinc.h.ORIGINAL
-mv 3rdparty/include/SDL2/SDL_stdinc.h.ORIGINAL 3rdparty/MACOS_SUPPORT
-mv 3rdparty/MACOS_SUPPORT/SDL_stdinc.h 3rdparty/include/SDL2
-echo "DONE!"
