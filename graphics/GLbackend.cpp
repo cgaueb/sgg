6333339326ed64c22a0ff4ad9551c40fb6dd2dac
vineek <vitsas@aueb.gr>
2020-12-02 11:29:51 +0200

    Disable Multisampling and lower requested GL Version

--- graphics/GLbackend.cpp
+++ graphics/GLbackend.cpp
@@ -773,10 +773,10 @@ namespace graphics
 		m_context = SDL_GL_CreateContext(m_window);
 
 		
-		SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4);
+		//SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4);
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
 		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 		SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
 		SDL_GL_SetSwapInterval(0);
