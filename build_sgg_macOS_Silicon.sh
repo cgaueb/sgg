b5e691110041d47db5ddb752afee2c0f8f18bca8
Dimitris Marakomihelakis <dmarakom@gmail.com>
2025-12-09 16:45:59 +0200

    feat: Add macOS Silicon build scripts for sgg library and demo.

---
+++ build_sgg_macOS_Silicon.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# @dmarakom6 modifications of @gsiros's shell script build_sgg_macOS.sh for Silicon Macs
+
+# Install dependencies if missing
+echo "Checking dependencies..."
+brew install glew sdl2 sdl2_mixer freetype
+
+CC=g++
+LD=g++
+AR=ar
+BUILD_PATH="build/Release"
+BUILD_PATH_DEBUG="build/Debug"
+LIB_PATH="lib"
+CFLAGS="-O2 -arch x86_64"
+CFLAGS_DEBUG="-Og -g -arch x86_64"
+
+mkdir -p build
+mkdir -p $LIB_PATH
+mkdir -p $BUILD_PATH
+mkdir -p $BUILD_PATH_DEBUG
+mkdir -p $BUILD_PATH/sgg
+mkdir -p $BUILD_PATH_DEBUG/sgg
+
+echo "-- START OF SGG LIB (RELEASE) COMPILATION --"
+$CC -c -std=c++17 $CFLAGS -I. -I3rdparty/include sgg/graphics.cpp -o $BUILD_PATH/sgg/graphics.o
+echo "Compiled graphics!"
+$CC -c -std=c++17 $CFLAGS -I. -I3rdparty/include sgg/GLbackend.cpp -o $BUILD_PATH/sgg/GLbackend.o
+echo "Compiled GLbackend!"
+$CC -c -std=c++17 $CFLAGS -I. -I3rdparty/include sgg/shader.cpp -o $BUILD_PATH/sgg/shader.o
+echo "Compiled shader!"
+$CC -c -std=c++17 $CFLAGS -I. -I3rdparty/include sgg/texture.cpp -o $BUILD_PATH/sgg/texture.o
+echo "Compiled texture!"
+$CC -c -std=c++17 $CFLAGS -I. -I3rdparty/include sgg/audio.cpp -o $BUILD_PATH/sgg/audio.o
+echo "Compiled audio!"
+$CC -c -std=c++17 $CFLAGS -I. -I3rdparty/include sgg/AudioManager.cpp -o $BUILD_PATH/sgg/AudioManager.o
+echo "Compiled AudioManager!"
+$CC -c -std=c++17 $CFLAGS -I. -I3rdparty/include sgg/lodepng.cpp -o $BUILD_PATH/sgg/lodepng.o
+echo "Compiled lodepng!"
+$CC -c -std=c++17 $CFLAGS -I. -I3rdparty/include sgg/fonts.cpp -o $BUILD_PATH/sgg/fonts.o
+echo "Compiled fonts!"
+
+$AR rcs $LIB_PATH/libsgg.a $BUILD_PATH/sgg/*.o
+
+echo "-- START OF SGG LIB (DEBUG) COMPILATION --"
+$CC -c -std=c++17 $CFLAGS_DEBUG -I. -I3rdparty/include sgg/graphics.cpp -o $BUILD_PATH_DEBUG/sgg/graphics.o
+echo "Compiled graphics!"
+$CC -c -std=c++17 $CFLAGS_DEBUG -I. -I3rdparty/include sgg/GLbackend.cpp -o $BUILD_PATH_DEBUG/sgg/GLbackend.o
+echo "Compiled GLbackend!"
+$CC -c -std=c++17 $CFLAGS_DEBUG -I. -I3rdparty/include sgg/shader.cpp -o $BUILD_PATH_DEBUG/sgg/shader.o
+echo "Compiled shader!"
+$CC -c -std=c++17 $CFLAGS_DEBUG -I. -I3rdparty/include sgg/texture.cpp -o $BUILD_PATH_DEBUG/sgg/texture.o
+echo "Compiled texture!"
+$CC -c -std=c++17 $CFLAGS_DEBUG -I. -I3rdparty/include sgg/audio.cpp -o $BUILD_PATH_DEBUG/sgg/audio.o
+echo "Compiled audio!"
+$CC -c -std=c++17 $CFLAGS_DEBUG -I. -I3rdparty/include sgg/AudioManager.cpp -o $BUILD_PATH_DEBUG/sgg/AudioManager.o
+echo "Compiled AudioManager!"
+$CC -c -std=c++17 $CFLAGS_DEBUG -I. -I3rdparty/include sgg/lodepng.cpp -o $BUILD_PATH_DEBUG/sgg/lodepng.o
+echo "Compiled lodepng!"
+$CC -c -std=c++17 $CFLAGS_DEBUG -I. -I3rdparty/include sgg/fonts.cpp -o $BUILD_PATH_DEBUG/sgg/fonts.o
+echo "Compiled fonts!"
+
+$AR rcs $LIB_PATH/libsggd.a $BUILD_PATH_DEBUG/sgg/*.o
